9652097a1e59c632a8483828076bd73e531cf6cf
Jorge Sendejo <jorge00552075@gmail.com>
2022-03-01 21:55:10 -0600

    add login component to home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="*" element={} /> */}
+    </Routes>
   );
 }
 
